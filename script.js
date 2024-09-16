2c30bfb5e8977c1675d047d65ca9ffd625082129
Koki Rizov <kalo282828@gmail.com>
2024-09-16 15:56:23 +0300

    chore: Add dialog for adding new books and improve book card functionality

--- script.js
+++ script.js
@@ -1,34 +1,75 @@
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    return myLibrary.length - 1; // Return the index of the new book
+}
 
-function addBookToLibrary(library, book) {
-  library.push(book);
-    return 0;
-}
-function showBooks(library) {
-  for (let i = 0; i < library.length; i++) {
-    console.log(library[i]);
-  }
-}
-function createCard(title, author, description) {
+const addBookForm = document.getElementById('add-book-form');
+const addBookDialog = document.getElementById('add-book-dialog');
+const cardContainer = document.getElementById('card-container');
+
+function createCard(book, index) {
     return `
-        <div class="card">
-            <h3>${title}</h3>
-            <p><strong>Author:</strong> ${author}</p>
-            <p><strong>Description:</strong> ${description}</p>
+        <div class="card" data-index="${index}">
+            <h3>${book.title}</h3>
+            <p><strong>Author:</strong> ${book.author}</p>
+            <p><strong>Pages:</strong> ${book.pages}</p>
+            <p><strong>Read:</strong> ${book.read ? 'Yes' : 'No'}</p>
+            <button class="delete" onclick="removeBook(${index})">Remove book</button>
+            <button class="toggle-read" onclick="toggleRead(${index})">Toggle Read Status</button>
         </div>
     `;
 }
-function addCard(title, author, description) {
-    const cardContainer = document.getElementById('card-container');
-    const cardHTML = createCard(title, author, description);
+
+function addCard(title, author, pages, read) {
+    const index = addBookToLibrary(title, author, pages, read);
+    const cardHTML = createCard(myLibrary[index], index);
     cardContainer.insertAdjacentHTML('beforeend', cardHTML);
 }
 
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    updateDisplay();
+}
+
+function toggleRead(index) {
+    myLibrary[index].read = !myLibrary[index].read;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    cardContainer.innerHTML = '';
+    myLibrary.forEach((book, index) => {
+        const cardHTML = createCard(book, index);
+        cardContainer.insertAdjacentHTML('beforeend', cardHTML);
+    });
+}
+
+function showAddBookDialog() {
+    addBookDialog.showModal();
+}
+
+addBookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(addBookForm);
+    const title = formData.get('title');
+    const author = formData.get('author');
+    const pages = formData.get('pages');
+    const read = formData.get('read') === 'on';
+    addCard(title, author, pages, read);
+    addBookDialog.close();
+    addBookForm.reset();
+});
+
+window.showAddBookDialog = showAddBookDialog;
+window.removeBook = removeBook;
+window.toggleRead = toggleRead;
